495f1913ac83333621e5c2c24fe62ce32ed3f781
K-ai30 <artpirate11@aim.com>
2020-03-13 00:56:25 -0400

    html layouts continued

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- Trucks Table data
+INSERT INTO trucks
+    (truck_name)
+VALUES
+("Let's Taco Bout It");
+("Big Burgers and Wings");
+("The Patty Wagon");
+("Gekko Food Truck");
+("South of Philly");
+("Grubbin' Out");
+("Queen Churro");
+("Subzero Nitrogen Ice Cream");
+("The Royal Pig BBQ");
