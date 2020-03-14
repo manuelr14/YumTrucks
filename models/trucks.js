1f1edbfcac103c71d127de252f502a8a801becb5
K-ai30 <artpirate11@aim.com>
2020-03-14 10:28:47 -0400

    separated trucks and users models

--- models/trucks.js
+++ models/trucks.js
@@ -1,9 +1,8 @@
 // Requiring bcrypt for password hashing. Using the bcryptjs version as the regular bcrypt module sometimes causes errors on Windows machines
 var bcrypt = require("bcryptjs");
 // Creating our User model
-
 module.exports = function(sequelize, DataTypes) {
-  var  User = sequelize.define("user", {
+  var  Truck = sequelize.define("truck", {
     // The email cannot be null, and must be a proper email before creation
     email: {
       type: DataTypes.STRING,
@@ -18,15 +17,28 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
+    truck_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    menu: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     first_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     last_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    avatar: {
       type: DataTypes.STRING,
       allowNull: false,
     },
@@ -49,13 +61,14 @@ module.exports = function(sequelize, DataTypes) {
 
   });
   // Creating a custom method for our Truck model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  User.prototype.validPassword = function(password) {
+  Truck.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
   // Hooks are automatic methods that run during various phases of the Truck Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  User.addHook("beforeCreate", function(user) {
+  Truck.addHook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
-  return User;
+  return Truck;
 };
+
