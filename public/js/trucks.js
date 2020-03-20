e281a59f3215e34ec6918fbcc3050e8c53dc4a64
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-03-20 11:37:08 -0400

    delete this

--- public/js/trucks.js
+++ public/js/trucks.js
@@ -1,7 +1,22 @@
-$(document).ready(function() {
-    // This file just does a the JS for the trucks page to figure out what information gets populated in what areas
-    // and updates the HTML on the page
-    $.get("/api/trucks_data").then(function(data) {
-      $(".truck-name").text(data.email);
-    });
-  });  
+$(document).ready(function () {
+
+
+  $.get("/api/trucks", data => {
+
+    console.log(data);
+    //createTruckCards(data);
+
+  });
+  $.get("/api/trucks/favorites", data => {
+
+    console.log(data);
+    //createTruckCards(data);
+
+  });
+
+
+
+
+
+
+});  
