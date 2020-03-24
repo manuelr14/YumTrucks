eea4527ff60c3ef2e542af700f83bdd62798e267
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-03-24 18:01:22 -0400

    test

--- public/js/trucks.js
+++ public/js/trucks.js
@@ -9,22 +9,22 @@ const color = {
   },
 };
 
-$(document).ready(function() {
-    // This file just does a the JS for the trucks page to figure out what information gets populated in what areas
-    // and updates the HTML on the page
-    $.get("/api/trucks").then(function(data) {
-      console.log(data);
-      getTruck(data);
-  
+$(document).ready(function () {
+  // This file just does a the JS for the trucks page to figure out what information gets populated in what areas
+  // and updates the HTML on the page
+  $.get("/api/trucks").then(function (data) {
+    console.log(data);
+    getTruck(data);
 
-    });
+
   });
+});
 
 
 function getTruck(data) {
-console.log(data);
+  console.log(data);
 
-$("#trucks-info").empty();
+  $("#trucks-info").empty();
 
   for (let i = 0; i < data.length; i++) {
     // create a parent div for the oncoming elements
@@ -32,27 +32,27 @@ $("#trucks-info").empty();
 
     infoSection.addClass("card-panel cyan");
 
-    infoSection.attr("id", "info-"+i);
+    infoSection.attr("id", "info-" + i);
 
     $("#trucks-info").append(infoSection);
 
-  
-
-
-
-      $("#info-"+i).append("<h2>" + data[i].truck_name + "</h2>");
-
-      $("#info-"+i).append('<h4>Location: <a href="https://www.google.com/maps/place/' + data[i].street + ", " + data[i].city + ", " + data[i].state + ", " + data[i].zip + '"target="_blank"> ' + data[i].street + ", " + data[i].city + ", " + data[i].state + ", " + data[i].zip + '</a> </h4>');
-
-      $("#info-"+i).append('<h4>Menu: <a href="' + data[i].menu + '" target="_blank">' + data[i].menu+'</a> </h4>');
-
-      $("#info-"+i).append('<h4>Website: <a href="' + data[i].website + '"target="_blank"> ' + data[i].website + '</a> </h4>');
 
 
 
 
+    $("#info-" + i).append("<h2>" + data[i].truck_name + "</h2>");
 
-     }
+    $("#info-" + i).append('<h4>Location: <a href="https://www.google.com/maps/place/' + data[i].street + ", " + data[i].city + ", " + data[i].state + ", " + data[i].zip + '"target="_blank"> ' + data[i].street + ", " + data[i].city + ", " + data[i].state + ", " + data[i].zip + '</a> </h4>');
+
+    $("#info-" + i).append('<h4>Menu: <a href="' + data[i].menu + '" target="_blank">' + data[i].menu + '</a> </h4>');
+
+    $("#info-" + i).append('<h4>Website: <a href="' + data[i].website + '"target="_blank"> ' + data[i].website + '</a> </h4>');
+
+
+
+
+
+  }
 
 
 
@@ -62,13 +62,13 @@ $("#trucks-info").empty();
 
 
 function generateTruckCards(data) {
-      data.forEach(truck => {
-        $("#trucks-container").append(
-          //TODO 
-          //1. write a click event targeting the "truck-card" class (different func)
-          //2. use our data-name attribute to call our /api/trucks/<truck_name> route
-          //3. use the truck info we get from that to populate values into the modal
-          `<div class="truck-card col s12 m6 l3" data-name='${truck.name}'>
+  data.forEach(truck => {
+    $("#trucks-container").append(
+      //TODO 
+      //1. write a click event targeting the "truck-card" class (different func)
+      //2. use our data-name attribute to call our /api/trucks/<truck_name> route
+      //3. use the truck info we get from that to populate values into the modal
+      `<div class="truck-card col s12 m6 l3" data-name='${truck.name}'>
             <div class="card-panel cyan">
                 <h2>${truck.truck_name}</h2>
                 <div class="card-image center-align">
@@ -77,18 +77,18 @@ function generateTruckCards(data) {
                   <a href="${truck.website}">Website:</a>
               <br />
             </div>
-          </div>`     
-        )
-      })
+          </div>`
+    )
+  })
 }
 
 // <!-- Modal Structure -->
 // <div id="modal1" class="modal">
 //     <div class="modal-content" style= "color:black">
 //         <img id="tImage-0" class="float" src="https://www.fillmurray.com/200/300">
-        
+
 //         <h5 id="tName-0">Truck Name</h5>
-    
+
 //         <h6 id="tAvatar-0" ></h6>
 //         <h6 id="tMenu-0" >Menu:</h6>
 //         <h6 id="tLocation-0" >Location:</h6>
